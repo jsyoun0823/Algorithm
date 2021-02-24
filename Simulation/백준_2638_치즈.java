106816f229abcfed6e7af6c4221f1d8f7d5dd584
yjsun <yjsun_@naver.com>
2021-02-24 15:17:22 +0900

    7주차_ 백준 치즈

--- Simulation/백준_2638_치즈.java
+++ Simulation/백준_2638_치즈.java
@@ -4,9 +4,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 백준_2638_치즈 {
+
     static int N, M, cheese, ans;
     static int[][] map;
-    static Queue<int[]> q;
     static List<int[]> melted;
 
     public static void main(String[] args) throws IOException {
@@ -17,21 +17,20 @@ public class 백준_2638_치즈 {
         N = Integer.parseInt(st.nextToken()); // 모눈종이 크기
         M = Integer.parseInt(st.nextToken()); // (5≤N, M≤100)
         map = new int[N][M]; // N×M의 모눈종이, 0:치즈X, 1:치즈O
-        q = new LinkedList<>();
-
         cheese = 0; // 치즈 개수
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if(map[i][j] == 1) {
-                    q.offer(new int[] {i, j}); // 치즈 위치 저장
                     cheese++; // 치즈 개수 카운팅
                 }
             }
         }
 
-        ans = 0;
+        ans = 0; // 치즈가 다 녹는 데 걸린 시간
+
         while(true){
             if(cheese <= 0) break; // 치즈 다 녹으면 종료
 
@@ -52,7 +51,7 @@ public class 백준_2638_치즈 {
         System.out.println(ans);
     }
 
-    static int[] dr = {-1, 1, 0, 0};
+    static int[] dr = {-1, 1, 0, 0}; // 상 하 좌 우
     static int[] dc = {0, 0, -1, 1};
 
     // 외부공기, 내부공기 나누기 위해 BFS 탐색
@@ -68,9 +67,10 @@ public class 백준_2638_치즈 {
         Queue<int[]> air = new LinkedList<>();
         boolean[][] visited = new boolean[N][M];
 
-        air.offer(new int[] {0, 0});
+        air.offer(new int[] {0, 0}); // 0, 0부터 탐색
         visited[0][0] = true;
 
+        // BFS 탐색
         while(!air.isEmpty()) {
             int size = air.size();
             for (int i = 0; i < size; i++) {
@@ -81,6 +81,8 @@ public class 백준_2638_치즈 {
                     int nr = r + dr[k]; int nc = c + dc[k];
 
                     if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+
+                    if(visited[nr][nc]) continue;;
 
                     if(map[nr][nc] == 0) {
                         air.offer(new int[] {nr, nc});
@@ -101,46 +103,21 @@ public class 백준_2638_치즈 {
 
     // 치즈 4변 중 2변이 -1 이라면 녹는다
     public static void check_melt() {
-//        while(!q.isEmpty()) {
-//            int size = q.size();
-//            for (int i = 0; i < size; i++) {
-//                int[] cur = q.poll();
-//                int r = cur[0];
-//                int c = cur[1];
-//
-//                int cnt = 0; // 인접 빈칸 갯수
-//                for (int k = 0; k < dr.length; k++) {
-//                    int nr = r + dr[k];
-//                    int nc = c + dc[k];
-//
-//                    if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-//
-//                    if(map[nr][nc] == -1) cnt++;
-//                }
-//
-//                if(cnt >= 2) { // 2변 이상 공기 접촉이면 사라짐!
-//                    melted.add(new int[] {r, c});
-//                } else {
-//                    q.offer(new int[] {r, c}); // 사라질 치즈가 아니라면 다시 q에 넣기
-//                }
-//            }
-//        }
-
-        for (int i = 1; i < N-1; i++) {
+        for (int i = 1; i < N-1; i++) { // 가장자리는 공기니까 한칸 띄고 완전탐색
             for (int j = 1; j < M-1; j++) {
+                if(map[i][j] == 1) { // 치즈라면
+                    int cnt = 0; // 인접한 외부공기 갯수 카운팅할 변수
 
-                if(map[i][j] == 1) {
-                    int cnt = 0;
-                    for (int k = 0; k < dr.length; k++) {
+                    for (int k = 0; k < dr.length; k++) { // 인접 칸 돌면서
                         int nr = i + dr[k];
                         int nc = j + dc[k];
 
                         if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
 
-                        if(map[nr][nc] == -1) cnt++;
+                        if(map[nr][nc] == -1) cnt++; // 인접한 외부공기 갯수 체크
                     }
 
-                    if(cnt >= 2) {
+                    if(cnt >= 2) { // 맞닿은 외부공기 2칸 이상이면 녹아야됨
                         melted.add(new int[] {i, j});
                     }
                 }
