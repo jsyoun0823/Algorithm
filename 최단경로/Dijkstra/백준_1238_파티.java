1005347ef95e9cb80377c7911ab69db4854ec387
yjsun <yjsun_@naver.com>
2021-03-18 17:37:49 +0900

    Week#10

---
+++ 최단경로/Dijkstra/백준_1238_파티.java
@@ -0,0 +1,22 @@
+package Dijkstra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준_1238_파티 {
+    private static int N, M, X;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken()); 
+        X = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < M; i++) {
+            
+        }
+    }
+}
