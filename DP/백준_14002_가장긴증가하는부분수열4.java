5ab0cd5748a8c0b4157d5fe4dff4c603036f9c18
yjsun <yjsun_@naver.com>
2021-11-11 23:30:32 +0900

    211111 구현,dp

--- DP/백준_14002_가장긴증가하는부분수열4.java
+++ DP/백준_14002_가장긴증가하는부분수열4.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class 백준_14002_가장긴증가하는부분수열 {
+public class 백준_14002_가장긴증가하는부분수열4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
